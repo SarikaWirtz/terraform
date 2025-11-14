6bebd0a0b1e018031263fdbe2e41c711e2b75a97
Sarika Wirtz <sarikasingh223@gmail.com>
2025-11-14 18:07:54 +0100

    #2:Add ec2 instance

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.21.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  # Configuration options
+  region  = "us-west-2"
+  profile = "jack.roper"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04f9aa2b7c7091927"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
 }
